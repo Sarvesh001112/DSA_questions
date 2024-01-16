c450203846a587f00de09460276f2b86f3b3ccff
Sarvesh001112 <124238958+Sarvesh001112@users.noreply.github.com>
2024-01-16 16:31:37 +0530

    Add files via upload

---
+++ String/CharArrBasic.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // char name[100];           //declaration of charater array and input
+
+    // name[0]='s';
+    // name[1]='a';
+    // cin>>name[2];
+    // cout<<name[0]<<name[1]<<name[2]<<endl;
+
+
+    // cout<<" enter your name:"<<endl;
+    // cin>>name;
+    // cout<<"Your name is: "<<name<<endl;
+
+
+    //   cin >> name;
+    //    for(int i =0; i<7; i++){
+    //     cout<<"index"<<i<<"value: "<<name[i]<<endl;
+    //    }
+    //    int value= (int) name [7];
+    //    cout<<"value is : "<<value<<endl;
+
+           //                   ##########
+    
+    // char arr[100];
+    // cout<<"enter your full name: "<<endl;
+    // cin.getline(arr,50);
+    // cout<<arr;
+
+
+    return 0;
+}
